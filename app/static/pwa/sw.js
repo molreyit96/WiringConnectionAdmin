a6cd1aaf44e19e6861b773b0bbafd974f36e2268
Paulo <paulo.ismalej@gmail.com>
2026-09-23 22:26:14 -0600

    feat(Mobile): add PWA installability layer (manifest, icons, service worker)

---
+++ app/static/pwa/sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'wcapp-v1';
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  const request = event.request;
+  if (request.method !== 'GET') {
+    return;
+  }
+  event.respondWith(
+    fetch(request)
+      .then((response) => {
+        const copy = response.clone();
+        caches
+          .open(CACHE_NAME)
+          .then((cache) => cache.put(request, copy))
+          .catch(() => {});
+        return response;
+      })
+      .catch(() => caches.match(request))
+  );
+});
